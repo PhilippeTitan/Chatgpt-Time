fc7ab637b502cd998382328bd4ce7cbcd6cbdebe
PhilippeTitan <philijanathethird@gmail.com>
2026-01-30 20:52:55 -0500

    Convert Pulse to JSON API for Python tool compatibility

--- netlify/functions/time.js
+++ netlify/functions/time.js
@@ -1,18 +1,25 @@
-const crypto = require('crypto');
-
 exports.handler = async () => {
   const now = new Date();
-  const timeStr = new Intl.DateTimeFormat('en-US', {
-    timeZone: 'America/Port-au-Prince',
-    hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false
-  }).format(now);
+  const timeZone = 'America/Port-au-Prince';
+  
+  const h = new Intl.DateTimeFormat('en-US', { timeZone, hour: '2-digit', hour12: false }).format(now);
+  const m = new Intl.DateTimeFormat('en-US', { timeZone, minute: '2-digit' }).format(now);
+  const s = new Intl.DateTimeFormat('en-US', { timeZone, second: '2-digit' }).format(now);
+  const d = new Intl.DateTimeFormat('en-US', { timeZone, day: '2-digit' }).format(now);
+  const mo = new Intl.DateTimeFormat('en-US', { timeZone, month: '2-digit' }).format(now);
+  const y = new Intl.DateTimeFormat('en-US', { timeZone, year: 'numeric' }).format(now);
 
-  // Generate a completely random 10-char token
-  const token = crypto.randomBytes(5).toString('hex').toUpperCase();
+  const data = {
+    status: "SYNCED",
+    location: "Port-au-Prince, Haiti",
+    raw_haiti: { day: d, month: mo, year: y, hour: h, minute: m, second: s },
+    unix_ms: now.getTime(),
+    hash: Math.random().toString(36).substring(2, 7).toUpperCase()
+  };
 
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain", "Cache-Control": "no-cache" },
-    body: `TOKEN:[${token}] TIME:[${timeStr}]`
+    headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+    body: JSON.stringify(data)
   };
 };
